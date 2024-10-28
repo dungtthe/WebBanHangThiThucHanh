d8322cd02ea1bee582e51142af4d2bee5c524c62
Vu Binh <22520129@gm.uit.edu.vn>
2024-10-28 10:34:33 +0700

    XoaSanPham: đag làm

--- WebBanHang/Areas/Admin/Controllers/ProductController.cs
+++ WebBanHang/Areas/Admin/Controllers/ProductController.cs
@@ -10,160 +10,161 @@ using WebBanHang.Security;
 
 namespace WebBanHang.Areas.Admin.Controllers
 {
-    [Area("Admin")]
-    [AdminAuthorizationFilter]
-    [Route("admin/product")]
-    public class ProductController : Controller
-    {
-        private readonly WebDbContext _context;
+	[Area("Admin")]
+	[AdminAuthorizationFilter]
+	[Route("admin/product")]
+	public class ProductController : Controller
+	{
+		private readonly WebDbContext _context;
 
-        public ProductController(WebDbContext context)
-        {
-            _context = context;
-        }
+		public ProductController(WebDbContext context)
+		{
+			_context = context;
+		}
 
-        [HttpGet("")]
-        public async Task<IActionResult> Index()
-        {
-            var webDbContext = _context.Products.Include(p => p.Category);
-            return View(await webDbContext.ToListAsync());
-        }
+		[HttpGet("")]
+		public async Task<IActionResult> Index()
+		{
+			var webDbContext = _context.Products.Include(p => p.Category);
+			return View(await webDbContext.ToListAsync());
+		}
 
-        // GET: Admin/Product/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.Products == null)
-            {
-                return NotFound();
-            }
+		// GET: Admin/Product/Details/5
+		public async Task<IActionResult> Details(int? id)
+		{
+			if (id == null || _context.Products == null)
+			{
+				return NotFound();
+			}
 
-            var product = await _context.Products
-                .Include(p => p.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
+			var product = await _context.Products
+				.Include(p => p.Category)
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
 
-            return View(product);
-        }
+			return View(product);
+		}
 
-        [HttpGet("create")]
-        public IActionResult Create()
-        {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
-            return View();
-        }
+		[HttpGet("create")]
+		public IActionResult Create()
+		{
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+			return View();
+		}
 
-        [HttpPost("create")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,Price,Images,CategoryId")] Product product)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(product);
-                await _context.SaveChangesAsync();
-                TempData["Message"] = "Thêm sản phẩm " + product.Name + " thành công";
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
+		[HttpPost("create")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,Price,Images,CategoryId")] Product product)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(product);
+				await _context.SaveChangesAsync();
+				TempData["Message"] = "Thêm sản phẩm " + product.Name + " thành công";
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+			return View(product);
+		}
 
-        // GET: Admin/Product/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Products == null)
-            {
-                return NotFound();
-            }
+		// GET: Admin/Product/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null || _context.Products == null)
+			{
+				return NotFound();
+			}
 
-            var product = await _context.Products.FindAsync(id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
+			var product = await _context.Products.FindAsync(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+			return View(product);
+		}
 
-        // POST: Admin/Product/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Images,CategoryId")] Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
+		// POST: Admin/Product/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,Images,CategoryId")] Product product)
+		{
+			if (id != product.Id)
+			{
+				return NotFound();
+			}
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(product);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(product.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
-            return View(product);
-        }
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(product);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!ProductExists(product.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", product.CategoryId);
+			return View(product);
+		}
 
-        // GET: Admin/Product/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Products == null)
-            {
-                return NotFound();
-            }
+		// GET: Admin/Product/Delete/5
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null || _context.Products == null)
+			{
+				return NotFound();
+			}
 
-            var product = await _context.Products
-                .Include(p => p.Category)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
+			var product = await _context.Products
+				.Include(p => p.Category)
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (product == null)
+			{
+				return NotFound();
+			}
 
-            return View(product);
-        }
+			return View(product);
+		}
 
-        // POST: Admin/Product/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.Products == null)
-            {
-                return Problem("Entity set 'WebDbContext.Products'  is null.");
-            }
-            var product = await _context.Products.FindAsync(id);
-            if (product != null)
-            {
-                _context.Products.Remove(product);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+		// POST: Admin/Product/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			if (_context.Products == null)
+			{
+				return Problem("Entity set 'WebDbContext.Products'  is null.");
+			}
+			var product = await _context.Products.FindAsync(id);
+			if (product != null)
+			{
+				_context.Products.Remove(product);
+				TempData["Message"] = "Xóa sản phẩm " + product.Name + " thành công.";
+			}
 
-        private bool ProductExists(int id)
-        {
-          return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
-        }
-    }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
+
+		private bool ProductExists(int id)
+		{
+			return (_context.Products?.Any(e => e.Id == id)).GetValueOrDefault();
+		}
+	}
 }
